8a2cc658f9957cc3ae3d795b6e74ec1b2194c08f
Geoffrey KEFF <geoffrey.keff@hotmail.com>
2024-04-03 10:10:10 +0200

    correction mongo

--- MicroService/GestionFreezbe/dist/index.js
+++ MicroService/GestionFreezbe/dist/index.js
@@ -3,25 +3,27 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// @ts-ignore
 const express_1 = __importDefault(require("express"));
-// @ts-ignore
 const dotenv_1 = __importDefault(require("dotenv"));
+const cors_1 = __importDefault(require("cors"));
+// Assure-toi que tu as installé et configuré tes modèles de base de données et les routes correctement.
+// Pour `db`, tu devrais avoir un fichier `dbconnexion.ts` qui exporte une fonction ou un objet pour la connexion.
+// db connection
+const db = require("./models/dbconnexion");
+const FreezbeRoute = require("./routes/freezbe.routes");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 const port = process.env.PORT || 8000;
-const cors = require('cors');
-app.use(cors({
-  origin: 'http://localhost:3000',
-  methods: ['GET', 'POST'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+// Configuration CORS pour permettre les requêtes de l'origine du front-end
+app.use((0, cors_1.default)({
+    origin: 'http://localhost:3000', // Remplace avec l'origine réelle de ton front-end
+    credentials: true, // Permet les requêtes avec des identifiants
 }));
-
-// db connection
-const db = require("./models/dbconnexion");
 app.get('/', (req, res) => {
     res.json({ message: "Freezbe microservice" });
 });
-const FreezbeRoute = require("./routes/freezbe.routes");
 app.use("/freezbe", FreezbeRoute);
+app.listen(port, () => {
+    console.log(`Freezbe microservice is running on port ${port}`);
+});
--- MicroService/GestionFreezbe/dist/models/dbconnexion.js
+++ MicroService/GestionFreezbe/dist/models/dbconnexion.js
@@ -1,16 +1,16 @@
 "use strict";
 // @ts-ignore
 const mongoose = require('mongoose');
-//URL de la bdd
+// URL de la bdd mise à jour pour utiliser le nom du service Docker Compose pour MongoDB
 mongoose.set('strictQuery', true);
-//Connexion à la bdd
+// Connexion à la bdd avec le nom du service MongoDB spécifié dans docker-compose.yml
 mongoose.connect("mongodb://localhost:27017/freezbes", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 // @ts-ignore
 const db = mongoose.connection;
-//Message de connexion
+// Message de connexion
 db.on('error', console.error.bind(console, 'Erreur lors de la connexion'));
 db.once('open', function () {
     console.log("Connexion à la base OK");
